4721976f4f0af4895594b806e5b33ed7e1f7c09d
yaegassy <yaegassy@users.noreply.github.com>
2023-06-19 17:04:40 +0900

    feat: adjust and respect the template_path setting in config file (#19)

--- src/doqEngine.ts
+++ src/doqEngine.ts
@@ -3,9 +3,27 @@ import { ExtensionContext, OutputChannel, Range, TextDocument, Uri, window, work
 import cp from 'child_process';
 import fs from 'fs';
 import path from 'path';
+
+import ini from 'ini';
 import tmp from 'tmp';
+import toml from 'toml';
 
 import { getDoqPath } from './common';
+
+type DoqTemplatePath = {
+  doq?: {
+    template_path?: string;
+  };
+};
+
+type SetupCfgForDoqTemplatePath = DoqTemplatePath;
+
+type PyprojectTomlForDoqTemplatePath = {
+  tool?: DoqTemplatePath;
+};
+
+const WORKSPACE_PYPROJECT_TOML_PATH = path.join(workspace.root, 'pyproject.toml');
+const WORKSPACE_SETUP_CFG_PATH = path.join(workspace.root, 'setup.cfg');
 
 export async function doFormat(
   context: ExtensionContext,
@@ -55,6 +73,11 @@ export async function doFormat(
       // relative path
       args.push('--template_path', path.join(workspace.root, templatePath));
     }
+  }
+
+  const templatePathFromConfigFile = getDoqTemplatePathFromConfigFile();
+  if (!templatePath && templatePathFromConfigFile) {
+    args.push('--template_path', templatePathFromConfigFile);
   }
 
   if (isIgnoreException) {
@@ -112,4 +135,46 @@ export async function doFormat(
       resolve(text);
     });
   });
+}
+
+function getDoqTemplatePathFromConfigFile(): string | undefined {
+  let templatePath: string | undefined;
+
+  if (fs.existsSync(WORKSPACE_PYPROJECT_TOML_PATH)) {
+    // pyproject.toml
+    try {
+      const fileStr = fs.readFileSync(WORKSPACE_PYPROJECT_TOML_PATH, { encoding: 'utf-8' });
+      const data: PyprojectTomlForDoqTemplatePath = toml.parse(fileStr);
+      if (data.tool?.doq?.template_path) {
+        templatePath = data.tool.doq.template_path;
+      }
+    } catch (e) {
+      window.showErrorMessage('Failed to load TOML file. There may be a syntax error.');
+      throw e;
+    }
+  } else if (fs.existsSync(WORKSPACE_SETUP_CFG_PATH)) {
+    // setup.cfg
+    try {
+      const fileStr = fs.readFileSync(WORKSPACE_SETUP_CFG_PATH, { encoding: 'utf-8' });
+      const data: SetupCfgForDoqTemplatePath = ini.parse(fileStr);
+      if (data.doq?.template_path) {
+        templatePath = data.doq.template_path;
+      }
+    } catch (e) {
+      window.showErrorMessage('Failed to load INI file. There may be a syntax error.');
+      throw e;
+    }
+  }
+
+  if (templatePath) {
+    if (templatePath.startsWith('/')) {
+      // absolute path
+      return templatePath;
+    } else {
+      // relative path
+      return path.join(workspace.root, templatePath);
+    }
+  }
+
+  return templatePath;
 }
